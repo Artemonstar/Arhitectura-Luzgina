63111c75aa372c6a92dffb2d2c79b8df4a9d43d3
Попов Артём <TaurPIT@yandex.ru>
2026-03-03 20:48:03 +0300

    Lav 3_Zad 1-3

---
+++ Zada4a1/Zada4a1/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace Zada4a1
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
