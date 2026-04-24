be5f55f2c7d0c35784089d268482df546c8eb7ab
Попов Артём <TaurPIT@yandex.ru>
2026-04-24 10:53:10 +0300

    Добавлены две задачи(приветствие и калькулятор)

---
+++ Zad 1-2 PrivPoImeni/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 1
+            SayHelloTo("Tom");
+            SayHelloTo("Bob");
+            SayHelloTo("Alice");
+        }
+
+        static void SayHelloTo(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
---
+++ Zad 2-1 KalkulVozr/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            CalculateAge(1990, 2024);
+            CalculateAge(2005, 2024);
+
+            // вызов в сокращённой форме
+            CalculateAgeShort(1990, 2024);
+        }
+
+        static void CalculateAge(int birthYear, int currentYear)
+        {
+            int age = currentYear - birthYear;
+            Console.WriteLine($"Возраст: {age} лет");
+        }
+
+        // Сокращённая форма 
+        static void CalculateAgeShort(int birthYear, int currentYear) =>
+            Console.WriteLine($"Возраст: {currentYear - birthYear} лет");
+    }
+}
